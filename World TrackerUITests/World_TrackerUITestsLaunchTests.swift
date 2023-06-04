d2789c610a600272c87494df9141d2e7b3dcf265
Lucia Adams <lucia.pettway@thinkcompany.com>
2023-06-04 16:50:29 -0400

    Initial Commit

---
+++ World TrackerUITests/World_TrackerUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  World_TrackerUITestsLaunchTests.swift
+//  World TrackerUITests
+//
+//  Created by Lucia Pettway on 6/4/23.
+//
+
+import XCTest
+
+final class World_TrackerUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}

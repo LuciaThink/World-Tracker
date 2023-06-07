de8f1766b754c5b122a9572490f474ef3c064983
Lucia Adams <lucia.pettway@thinkcompany.com>
2023-06-06 20:22:23 -0400

    add additional shape examples

--- World Tracker/ViewController.swift
+++ World Tracker/ViewController.swift
@@ -2,7 +2,7 @@
 //  ViewController.swift
 //  World Tracker
 //
-//  Created by Lucia Pettway on 6/4/23.
+//  Created by Lucia Adams on 6/4/23.
 //
 
 import UIKit
@@ -15,16 +15,15 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         self.sceneView.debugOptions = [ARSCNDebugOptions.showFeaturePoints, ARSCNDebugOptions.showWorldOrigin]
-        
         self.sceneView.session.run(configuration)
         self.sceneView.autoenablesDefaultLighting = true
-        // Do any additional setup after loading the view.
     }
 
     @IBAction func reset(_ sender: Any) {
         self.restartSession()
     }
     
+    // Restart the scene view to update the origin
     func restartSession() {
         self.sceneView.session.pause()
         self.sceneView.scene.rootNode.enumerateChildNodes { (node, _) in
@@ -33,6 +32,7 @@ class ViewController: UIViewController {
         self.sceneView.session.run(configuration, options: [.resetTracking, .removeExistingAnchors])
     }
     
+    // Generate a random  number between two numbers
     func randomNumbers(firstNum: CGFloat, secondNum: CGFloat) -> CGFloat {
         return CGFloat(arc4random()) / CGFloat(UINT32_MAX) * abs(firstNum - secondNum) + min(firstNum, secondNum)
     }
@@ -43,11 +43,35 @@ class ViewController: UIViewController {
         let y = randomNumbers(firstNum: -0.3, secondNum: 0.3)
         let z = randomNumbers(firstNum: -0.3, secondNum: 0.3)
         
-        node.geometry = SCNBox(width: 0.1, height: 0.1, length: 0.1, chamferRadius: 0.03)
+        // Capsule Example
+        //node.geometry = SCNCapsule(capRadius: 0.1, height: 0.3)
+        
+        // Cone Example
+        // node.geometry = SCNCone(topRadius: 0.1, bottomRadius: 0.3, height: 0.3)
+        
+        // Sphere Example
+        // node.geometry = SCNSphere(radius: 0.3)
+        
+        // Torus Example
+        //node.geometry = SCNTorus(ringRadius: 0.3, pipeRadius: 0.1)
+        
+        // Plane Example
+        node.geometry = SCNPlane(width: 0.2, height: 0.2)
+        
+        // Quad Example
+        // node.geometry = SCNBox(width: 0.1, height: 0.1, length: 0.1, chamferRadius: 0.03)
+        
+        // Set materials and colors
         node.geometry?.firstMaterial?.specular.contents = UIColor.white
         node.geometry?.firstMaterial?.diffuse.contents = UIColor.green
-        node.position = SCNVector3(x, y, z)
         
+        // Randomize position
+        // node.position = SCNVector3(x, y, z)
+        
+        // Static Position
+        node.position = SCNVector3(0, 0, -0.3)
+        
+        // Append the node to the rootnode of the scene
         self.sceneView.scene.rootNode.addChildNode(node)
     }
 }
